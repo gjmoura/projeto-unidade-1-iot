77eacd0e893a5ab803ebe8acdf369dc3f25e0c28
Gustavo Jeronimo <gustavo.jeronimo@hotmail.com>
2021-07-27 17:23:53 -0300

    Adiciona o dashboard liagado ao servidor

---
+++ dash.js
@@ -0,0 +1,28 @@
+
+const liga = (id) => {
+  document.getElementById(id).setAttribute("src", "images/Lon.png");
+  document.getElementById("result").innerHTML= "Lâmpada Acesa!";
+}
+
+const desliga = (id) => {
+  document.getElementById(id).setAttribute("src", "images/Loff.png");
+  document.getElementById("result").innerHTML= "Lâmpada Apagada!";
+}
+
+const mudar = () => {
+  for(let i = 0; i < 10; i++) {
+    console.log("Mudou!");
+  }
+}
+
+function sendToServer(state) {
+  var KEY = "3FRETIHT52FB1D9G";
+  //criar um objeto capaz de enviar dados via requisição HTTP GET
+  const http = new XMLHttpRequest();
+  //prepara um GET passando a váriavel lux como ultimo paramentro do link
+  http.open("GET",`https://api.thingspeak.com/update?api_key=${KEY}&field1=0${state}`);
+  //envia um GET
+  http.send();
+  //quando a requisição retornar ele chama o console e imprime o valor gerado
+  http.onload = console.log(http.responseText+" "+state)
+}
